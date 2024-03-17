ed3fe4409ba1f6e8e5363e3cc61eb2fcc4cf0fe8
JohnKenneth <fetizanankennetho@gmail.com>
2024-03-17 15:01:09 +0800

    1st

---
+++ excercise.js
@@ -0,0 +1,42 @@
+async function main() {
+    const student = await getStudentAsync(1);
+    console.log('Student: ', student);
+    if (student.isEnrolled) {
+      const courses = await getCourseListAsync();
+      console.log('Course List: ', courses);
+      await sendNotificationAsync(student.email, courses);
+      console.log('Notification sent...');
+    }
+  }
+  
+  async function getStudentAsync(id) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({
+          id: 1,
+          name: 'John Kenneth Fetizanan',
+          isEnrolled: true,
+          email: 'john.ken@example.com',
+        });
+      }, 4000);
+    });
+  }
+  
+  async function getCourseListAsync() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(['course1', 'course2']);
+      }, 4000);
+    });
+  }
+  
+  async function sendNotificationAsync(email, courses) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 4000);
+    });
+  }
+  
+  main();
+  
